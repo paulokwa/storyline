76b8aa8556c0be599eca1ce16bb74c3bf69356df
paulokwa <skytra7@gmail.com>
2026-04-21 21:16:24 -0300

    feat: update showcase page to be available in all environments

--- app/dev/showcase/page.tsx
+++ app/dev/showcase/page.tsx
@@ -1,10 +1,5 @@
-import { notFound } from 'next/navigation'
 import Showcase from '@/components/marketing/Showcase'
 
 export default function DevShowcasePage() {
-  if (process.env.NODE_ENV !== 'development') {
-    notFound()
-  }
-
   return <Showcase />
 }
